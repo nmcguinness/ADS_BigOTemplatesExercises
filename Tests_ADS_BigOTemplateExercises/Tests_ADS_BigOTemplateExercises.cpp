7d87cb129cc0390e789fd10fa456185ee941ea75
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-11 09:41:11 +0000

    Added unit test code

---
+++ Tests_ADS_BigOTemplateExercises/Tests_ADS_BigOTemplateExercises.cpp
@@ -0,0 +1,61 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ADS_BigOTemplatesExercises/GenericArray.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestsADSBigOTemplateExercises
+{
+	TEST_CLASS(TestsGenericArray)
+	{
+	public:
+
+		TEST_METHOD(TestConstructor)
+		{
+		}
+
+		TEST_METHOD(TestSize)
+		{
+		}
+
+		TEST_METHOD(TestGetPass)
+		{
+		}
+
+		TEST_METHOD(TestGetOutOfLimits)
+		{
+		}
+
+		//TEST_METHOD(TestConstructor)
+		//{
+		//	bool result = false;
+		//	Assert::AreEqual(result, false);
+
+		//	std::string x = "xyz";
+		//	std::string y = "xyz";
+		//	//tests for unique and distinct memory addresses
+		//	Assert::AreNotSame(x, y);
+		//	Assert::AreEqual(x, y);
+		//}
+	};
+}
+
+/*
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestsADSBigOTemplateExercises
+{
+	TEST_CLASS(TestsADSBigOTemplateExercises)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
+
+*/
