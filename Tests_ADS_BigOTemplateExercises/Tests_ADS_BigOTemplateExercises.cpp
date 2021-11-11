bba6f6de7084d9ce79f41e85a6fa393ec5220712
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-11 09:55:42 +0000

    Added comments to unit tests

--- Tests_ADS_BigOTemplateExercises/Tests_ADS_BigOTemplateExercises.cpp
+++ Tests_ADS_BigOTemplateExercises/Tests_ADS_BigOTemplateExercises.cpp
@@ -3,27 +3,39 @@
 #include "../ADS_BigOTemplatesExercises/GenericArray.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace TestsADSBigOTemplateExercises
 {
 	TEST_CLASS(TestsGenericArray)
 	{
 	public:
+		string userName = "jane"; //["","","","",""]
 
 		TEST_METHOD(TestConstructor)
 		{
+			GenericArray<string> strArray(5, "default");
+			Assert::IsNotNull(&strArray); //get the address of object in RAM (because thats what a pointer is)
+
+			//or we could do same as line 17 but slightly more clumsily
+			//GenericArray<string>* pArray = &strArray;
+			//Assert::IsNotNull(pArray);
 		}
 
 		TEST_METHOD(TestSize)
 		{
+			//ask the user for input?
 		}
 
-		TEST_METHOD(TestGetPass)
+		TEST_METHOD(TestGetPass)  //0 <= index < length
 		{
+			//get(index)
 		}
 
 		TEST_METHOD(TestGetOutOfLimits)
 		{
+			//get(-1)
+			//get(length+1)
 		}
 
 		//TEST_METHOD(TestConstructor)
